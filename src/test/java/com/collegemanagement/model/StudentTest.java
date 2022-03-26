9b4af29f8544d6b4f59d2aa8ff78696d05a87eb7
lavanya-kumararaja <klavanya1997@gmail.com>
2022-03-26 14:52:34 +0530

    created Student model

---
+++ src/test/java/com/collegemanagement/model/StudentTest.java
@@ -0,0 +1,13 @@
+package com.collegemanagement.model;
+
+public class StudentTest {
+public static void main(String[] args) {
+	Student student=new Student();
+	student.setId(1);
+	student.setName("Lavanya");
+	student.setEmail("klavanya1997@gmail.com");
+	student.setMobile(8778804219l);
+	student.setPassword("Lalu@123");
+	System.out.println(student);
+}
+}
